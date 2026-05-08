1c7748c1cb029a06acb6596fd399476ec7f7270f
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-05-08 23:41:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your code here
 const nameInput = document.getElementById("item-name-input");
 const priceInput = document.getElementById("item-price-input");
-const addBtn = document.querySelector("button");
-const tableBody = document.querySelector("tbody");
+const addBtn = document.getElementById("add");
+const tableBody = document.getElementById("table-body");
 const grandTotalEl = document.getElementById("total");
 
 let total = 0;
@@ -11,28 +11,36 @@ addBtn.addEventListener("click", function () {
 	let name = nameInput.value.trim();
 	let price = parseFloat(priceInput.value);
 
+	// Input valid check
 	if(name === "" || isNaN(price) || price <= 0) {
 		return;
 	}
 
+	// Create row
 	const row = document.createElement("tr");
-	
+
+	// item cell
 	const nameCell = document.createElement("td");
 	nameCell.setAttribute("id", "item");
 	nameCell.innerText = name;
 
+	// price cell
 	const priceCell = document.createElement("td");
 	priceCell.setAttribute("id", "price");
 	priceCell.innerText = price;
 
+	// append cells
 	row.appendChild(nameCell);
 	row.appendChild(priceCell);
 
+	// add row to table
 	tableBody.appendChild(row);
 
+	// update total
 	total = total + price;
-	grandTotalEl.innerText = total;
+	grandTotalEl.innerText = `Grand Total: ${total}`;
 
+	// Clear inputs
 	nameInput.value = "";
 	priceInput.value = "";
 })
