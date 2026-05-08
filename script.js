9128ecf30d7f0ff9f8000d8b9a645abfeae20e3d
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-05-08 23:55:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your code here
 const nameInput = document.getElementById("item-name-input");
+const qtyInput = document.getElementById("item-qty-input");
 const priceInput = document.getElementById("item-price-input");
 const addBtn = document.getElementById("add");
 const tableBody = document.getElementById("table-body");
@@ -9,38 +10,35 @@ let total = 0;
 
 addBtn.addEventListener("click", function () {
 	let name = nameInput.value.trim();
+	let qty = parseInt(qtyInput.value);
 	let price = parseFloat(priceInput.value);
 
 	// Input valid check
-	if(name === "" || isNaN(price) || price <= 0) {
+	if(name === "" || isNaN(qty) || qty <= 0 || isNaN(price) || price <= 0) {
 		return;
 	}
 
+	let itemTotal = qty * price;
+	
 	// Create row
 	const row = document.createElement("tr");
 
-	// item cell
-	const nameCell = document.createElement("td");
-	nameCell.setAttribute("id", "item");
-	nameCell.innerText = name;
-
-	// price cell
-	const priceCell = document.createElement("td");
-	priceCell.setAttribute("id", "price");
-	priceCell.innerText = price;
-
-	// append cells
-	row.appendChild(nameCell);
-	row.appendChild(priceCell);
+	row.innerHTML = `
+		<td>${name}</td>
+		<td>${qty}</td>
+		<td>${price}</td>
+		<td>${itemTotal}</td>
+	`;
 
 	// add row to table
 	tableBody.appendChild(row);
 
 	// update total
-	total = total + price;
+	total += itemTotal;
 	grandTotalEl.innerText = `Grand Total: ${total}`;
 
 	// Clear inputs
 	nameInput.value = "";
+	qtyInput.value = "";
 	priceInput.value = "";
 })
